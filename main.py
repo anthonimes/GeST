cd21e2b4702398243d29d57d89d178395bb9f27d
anthonimes <anthony.perez@uinv-orleans.fr>
2021-01-01 11:05:01 +0100

    modifying imports

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# TEMPORARY
-from os import walk, makedirs 
-from src.helper import _parse_args, _savepreseg, _savefig, _colors
-
-import warnings
-warnings.filterwarnings("ignore")
-
-import src.gest
-
 def _write(g,arguments):
     from networkx import write_gpickle, write_weighted_edgelist
     from numpy import save
     from pickle import dump
+    from os import walkdirs
+    from src.helper import _savepreseg, _savefig
 
     _spatial_radius=float(arguments['hs']) #hs
     _range_radius=float(arguments['hr']) #hr
@@ -43,10 +36,10 @@ def _write(g,arguments):
     _savefig(g._segmentation, g._image, path_segmentation+str(i+1)+"_"+filename[:-4]+"_"+str(g._number_of_regions)+".png")
 
 def _display(g, arguments):
-    import skimage.segmentation
-    import skimage.color
-    import skimage.measure
-    import matplotlib.pyplot as plt
+    from skimage import color
+    from skimage import measure
+    from matplotlib import pyplot as plt
+    from src.helper import _colors
 
     if(arguments['merge']):
         fig, ax = plt.subplots(3, 2, figsize=(12, 8), sharex=True, sharey=True)
@@ -58,22 +51,22 @@ def _display(g, arguments):
     ax[0][1].imshow(g._presegmentation)
     ax[0][1].set_title("initial segmentation")
         
-    colored_regions = skimage.color.label2rgb(g._segmentation, g._image, alpha=1, colors=_colors(g._segmentation,g._image), bg_label=0)
+    colored_regions = color.label2rgb(g._segmentation, g._image, alpha=1, colors=_colors(g._segmentation,g._image), bg_label=0)
     ax[1][0].imshow(g._segmentation)
     ax[1][0].set_title('final segmentation')
-    colored_regions = skimage.color.label2rgb(g._segmentation, g._image, alpha=1, colors=_colors(g._segmentation,g._image), bg_label=0)
+    colored_regions = color.label2rgb(g._segmentation, g._image, alpha=1, colors=_colors(g._segmentation,g._image), bg_label=0)
     ax[1][1].imshow(colored_regions)
     ax[1][1].set_title('colored final segmentation')
 
     if(arguments['merge']):
         ax[2][0].imshow(g._segmentation_merged)
         ax[2][0].set_title('merged segmentation')
-        colored_regions = skimage.color.label2rgb(g._segmentation_merged, g._image, alpha=1, colors=_colors(g._segmentation_merged,g._image), bg_label=0)
+        colored_regions = color.label2rgb(g._segmentation_merged, g._image, alpha=1, colors=_colors(g._segmentation_merged,g._image), bg_label=0)
         ax[2][1].imshow(colored_regions)
         ax[2][1].set_title('colored merged segmentation')
 
         # ===== START DEBUG =====
-        regions = skimage.measure.regionprops(g._segmentation_merged)
+        regions = measure.regionprops(g._segmentation_merged)
         for region in regions:
             xy = region.centroid
             x = xy[1]
@@ -88,7 +81,12 @@ def _display(g, arguments):
     plt.show()
 
 if __name__ == "__main__":
-    
+    from os import walk
+    from src.helper import _parse_args
+    import warnings
+    warnings.filterwarnings("ignore")
+    import src.gest
+
     # construct the argument parser and parse the arguments
     # common arguments
     # TODO: should maybe return the GeST instance?
